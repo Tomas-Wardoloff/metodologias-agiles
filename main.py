3bb05d5042e8d366227dfe3e76ecdabc4c0bd848
Tomas-Wardoloff <tomaswardoloff@gmail.com>
2026-08-24 19:36:16 -0300

    REFACTOR: el rover informa posicion y orientacion

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 class Rover:
+    def __init__(self, posicion=(0, 0), orientacion="N"):
+        self.posicion = posicion
+        self.orientacion = orientacion
+
     def posicion_y_orientacion_iniciales(self):
-        return ((0, 0), "N")
+        return (self.posicion, self.orientacion)
