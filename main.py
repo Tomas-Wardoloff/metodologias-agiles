2ccc73364662a472cac2a0b09a042f9006357417
felipe zurita <felipeizurita@hotmail.com>
2026-08-24 20:01:34 -0300

    REFACTOR: Test rover orientacion

--- main.py
+++ main.py
@@ -6,6 +6,24 @@ class Rover:
     def posicion_y_orientacion_iniciales(self):
         return (self.posicion, self.orientacion)
 
-    def rotar_orientacion(self):
-        self.orientacion = "W"
+    def rotar_orientacion(self, giro):
+       
+        if self.orientacion == "N" and giro == "L":
+            self.orientacion = "W"
+        elif self.orientacion == "N" and giro == "R":
+            self.orientacion = "E"
+        elif self.orientacion == "E" and giro == "L":
+            self.orientacion = "N"
+        elif self.orientacion == "E" and giro == "R":
+            self.orientacion = "N"
+        elif self.orientacion == "W" and giro == "L":
+                self.orientacion = "S"
+        elif self.orientacion == "W" and giro == "R":
+                self.orientacion = "N"
+        elif self.orientacion == "S" and giro == "L":
+                self.orientacion = "W"
+        elif self.orientacion == "S" and giro == "R":
+                self.orientacion = "E"
+        
+
         return self.orientacion
--- test.py
+++ test.py
@@ -4,4 +4,5 @@ def test_informa_posicion_y_orientacion_iniciales():
     assert Rover().posicion_y_orientacion_iniciales() == ((0, 0), "N")
 
 def test_rover_gira():
-    assert Rover().rotar_orientacion() == "W"
+    assert Rover().rotar_orientacion("L") == "W"
+    assert Rover().rotar_orientacion("R") == "E"
